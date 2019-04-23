15d6c8d9a89972dc05b0e72fa851ea3453c082c7
teck soon <tecksoon.ng@creditculture.com>
2019-04-23 19:31:35 +0800

    added binding

--- src/App.js
+++ src/App.js
@@ -122,9 +122,87 @@ function App() {
     }
   }
 
+  function UserGreeting(props) {
+    return <h1>Welcome back!</h1>;
+  }
+  
+  function GuestGreeting(props) {
+    return <h1>Please sign up.</h1>;
+  }
+
+  function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+      return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+  }
+
+  function LoginButton(props) {
+    return (
+      <button onClick={props.onClick}>
+        Login
+      </button>
+    );
+  }
+  
+  function LogoutButton(props) {
+    return (
+      <button onClick={props.onClick}>
+        Logout
+      </button>
+    );
+  }
+
+  class LoginControl extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleLoginClick = this.handleLoginClickbutton.bind(this);
+      this.handleLogoutClick = this.handleLogoutClickbutton.bind(this);
+      this.state = {isLoggedIn: false};
+    }
+  
+
+    handleLoginClickbutton() {
+      this.setState({isLoggedIn: true});
+    }
+  
+    handleLogoutClickbutton() {
+      this.setState({isLoggedIn: false});
+    }
+  
+    render() {
+      const isLoggedIn = this.state.isLoggedIn;
+      // let button;
+  
+      // if (isLoggedIn) {
+      //   button = <LogoutButton onClick={this.handleLogoutClick} />;
+      // } else {
+      //   button = <LoginButton onClick={this.handleLoginClick} />;
+      // }
+  
+      return (
+        // <div>
+        //   <Greeting isLoggedIn={isLoggedIn} />
+        //   {button}
+        // </div>
+        <div>
+          <Greeting isLoggedIn={isLoggedIn} />
+      {isLoggedIn ? (
+        <LogoutButton onClick={this.handleLogoutClick} />
+      ) : (
+        <LoginButton onClick={this.handleLoginClick} />
+      )}
+     
+    </div>
+      );
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+      <LoginControl />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
